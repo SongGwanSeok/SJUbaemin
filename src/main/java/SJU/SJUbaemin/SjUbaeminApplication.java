7573cd423f507a2f11e5f4f98d742b7ab794469b
GS_song <20003204@sju.ac.kr>
2023-02-02 16:43:53 +0900

    baemin clone project

---
+++ src/main/java/SJU/SJUbaemin/SjUbaeminApplication.java
@@ -0,0 +1,13 @@
+package SJU.SJUbaemin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjUbaeminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjUbaeminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SJU/SJUbaemin/SjUbaeminApplicationTests.java
@@ -0,0 +1,13 @@
+package SJU.SJUbaemin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjUbaeminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
