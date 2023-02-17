8474cae623c950ad7b7025bb438180714812edb9
thgee <102576089+thgee@users.noreply.github.com>
2023-02-17 17:58:30 +0900

    API 연결

--- views/src/App.js
+++ views/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 
+import axios from "axios";
+
 import Home from "./pages/Home";
 import Mungoo from "./pages/Mungoo";
 import Living from "./pages/Living";
@@ -13,23 +15,50 @@ import "./App.css";
 export const DataContext = React.createContext();
 
 function App() {
+  const SERVER_URL = "http://13.125.7.108:8080/api/";
   const [data, setData] = useState([]);
 
+  // ---------------- axios 사용 ----------------
+
+  // const getData = async () => {
+  //   const res = await axios.get("https://jsonplaceholder.typicode.com/photos");
+
+  //   const initData = res.data.slice(0, 20).map((it) => {
+  //     return {
+  //       img: it.url,
+  //       name: it.title.slice(0, 6),
+  //       price: 1000,
+  //       id: it.id,
+  //     };
+  //   });
+
+  //   setData(initData);
+  // };
+
+  // -------------fetch 사용 ----------------
+
+  // const getData = async () => {
+  //   const datas = await fetch(
+  //     "https://jsonplaceholder.typicode.com/photos"
+  //   ).then((res) => res.json());
+
+  //   const initData = datas.slice(0, 20).map((it) => {
+  //     return {
+  //       img: it.url,
+  //       name: it.title.slice(0, 6),
+  //       price: 1000,
+  //       id: it.id,
+  //     };
+  //   });
+
+  //   setData(initData);
+  // };
+
+  //  --------------- 세배돈 서버 API 연결 -----------------
+
   const getData = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/photos").then(
-      (res) => res.json()
-    );
-
-    const initData = res.slice(0, 20).map((it) => {
-      return {
-        img: it.url,
-        name: it.title.slice(0, 6),
-        price: 1000,
-        id: it.id,
-      };
-    });
-
-    setData(initData);
+    const { data } = await axios.get(`${SERVER_URL}product/all`);
+    setData(data.data);
   };
 
   useEffect(() => {
