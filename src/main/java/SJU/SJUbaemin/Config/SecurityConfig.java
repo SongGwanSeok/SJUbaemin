81ab7c3caa506549100faa764f0a65a78890b27c
GS_song <20003204@sju.ac.kr>
2023-02-21 15:26:09 +0900

    fix: cors error - CorsConfig

--- src/main/java/SJU/SJUbaemin/Config/SecurityConfig.java
+++ src/main/java/SJU/SJUbaemin/Config/SecurityConfig.java
@@ -14,18 +14,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity
 @EnableMethodSecurity
 @Configuration
 @RequiredArgsConstructor
-public class SecurityConfig implements WebMvcConfigurer {
+public class SecurityConfig {
 
     private final TokenProvider tokenProvider;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final CorsFilter corsFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -37,6 +38,8 @@ public class SecurityConfig implements WebMvcConfigurer {
         httpSecurity
                 // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
                 .csrf().disable()
+
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
 
                 // 예외 처리에 우리가 만든 클래스 추가
                 .exceptionHandling()
@@ -55,9 +58,11 @@ public class SecurityConfig implements WebMvcConfigurer {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
                 .and()
+                .cors()
+
+                .and()
                 .authorizeHttpRequests()
                 .requestMatchers(
-                        "/h2-console/**", "/favicon.ico",
                         "/api/hello",
                         "/api/authenticate", //토큰을 받기 위한 로그인 api
                         "/api/member/signup", // 회원가입을 하기 위한 회원가입 api
@@ -74,13 +79,4 @@ public class SecurityConfig implements WebMvcConfigurer {
         return httpSecurity.build();
     }
 
-    //cors 문제 해결
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("*")
-                .allowCredentials(true) // 쿠키 인증 요청 허용
-                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
-    }
 }
