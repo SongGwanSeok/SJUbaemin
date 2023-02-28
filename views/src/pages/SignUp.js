2e65e0b6e145c9170016f85e8c4d251222689075
bsu0404 <bsu0404@naver.com>
2023-02-28 11:58:21 +0900

    비밀번호 확인

--- views/src/pages/SignUp.js
+++ views/src/pages/SignUp.js
@@ -20,29 +20,42 @@ const SignUp = () => {
       <Header />
       <div className="content">
         <h2>회원정보</h2>
+        <div input_wrapper>
+          <Input type="text" onChange={setId} placeholder={"아이디"} />
+          <Input type="password" onChange={setPass} placeholder={"비밀번호"} />
+          <Input
+            type="password"
+            onChange={setCheckPass}
+            placeholder={"비밀번호 확인"}
+          />
+          <div className="same">
+            {pass === "" || checkPass === ""
+              ? ""
+              : pass === checkPass
+              ? "비밀번호가 일치합니다."
+              : "비밀번호가 일치하지 않습니다."}
+          </div>
+          <Input type="text" onChange={setName} placeholder={"이름"} />
+          <Input type="text" onChange={setEmail} placeholder={"이메일"} />
+          <Input type="text" onChange={setBirth} placeholder={"생일"} />
+          <Input type="text" onChange={setPhone} placeholder={"휴대폰번호"} />
+          <Input type="text" onChange={setAddress} placeholder={"주소"} />
 
-        <Input type="text" onChange={setId} placeholder={"아이디"} />
-        <Input type="password" onChange={setPass} placeholder={"비밀번호"} />
-        <Input
-          type="password"
-          onChange={setCheckPass}
-          placeholder={"비밀번호 확인"}
-        />
-        <Input type="text" onChange={setName} placeholder={"이름"} />
-        <Input type="text" onChange={setEmail} placeholder={"이메일"} />
-        <Input type="text" onChange={setBirth} placeholder={"생일"} />
-        <Input type="text" onChange={setPhone} placeholder={"휴대폰번호"} />
-        <Input type="text" onChange={setAddress} placeholder={"주소"} />
-        <button
-          onClick={() =>
-            addUser(id, pass, name, email, birth, phone, address).then(() => {
-              alert("회원가입 성공!");
-              navigate("/");
-            })
-          }
-        >
-          가입
-        </button>
+          <button
+            onClick={() =>
+              addUser(id, pass, name, email, birth, phone, address).then(
+                (res) => {
+                  console.log(res.status);
+                  // if (res.status === 200) {
+                  alert("회원가입 성공!");
+                  navigate("/");
+                }
+              )
+            }
+          >
+            가입
+          </button>
+        </div>
       </div>
     </div>
   );
