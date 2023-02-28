8f5493ee703ff5e4c1adf7c08868e9e8970e5584
thgee <xogur25@naver.com>
2023-02-28 12:33:12 +0900

    버그수정

--- views/src/pages/SignUp.js
+++ views/src/pages/SignUp.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import Input from "../components/Input";
-import addUser from "../utils/addUser";
+import AddUser from "../utils/addUser";
 
 const SignUp = () => {
   const [id, setId] = useState("");
@@ -20,42 +20,45 @@ const SignUp = () => {
       <Header />
       <div className="content">
         <h2>회원정보</h2>
-        <div input_wrapper>
-          <Input type="text" onChange={setId} placeholder={"아이디"} />
-          <Input type="password" onChange={setPass} placeholder={"비밀번호"} />
-          <Input
-            type="password"
-            onChange={setCheckPass}
-            placeholder={"비밀번호 확인"}
-          />
-          <div className="same">
-            {pass === "" || checkPass === ""
-              ? ""
-              : pass === checkPass
-              ? "비밀번호가 일치합니다."
-              : "비밀번호가 일치하지 않습니다."}
-          </div>
-          <Input type="text" onChange={setName} placeholder={"이름"} />
-          <Input type="text" onChange={setEmail} placeholder={"이메일"} />
-          <Input type="text" onChange={setBirth} placeholder={"생일"} />
-          <Input type="text" onChange={setPhone} placeholder={"휴대폰번호"} />
-          <Input type="text" onChange={setAddress} placeholder={"주소"} />
-
-          <button
-            onClick={() =>
-              addUser(id, pass, name, email, birth, phone, address).then(
-                (res) => {
-                  console.log(res.status);
-                  // if (res.status === 200) {
-                  alert("회원가입 성공!");
-                  navigate("/");
-                }
-              )
-            }
-          >
-            가입
-          </button>
+        <Input type="text" onChange={setId} placeholder={"아이디"} />
+        <Input type="password" onChange={setPass} placeholder={"비밀번호"} />
+        <Input
+          type="password"
+          onChange={setCheckPass}
+          placeholder={"비밀번호 확인"}
+        />
+        <div className="same">
+          {pass === "" || checkPass === ""
+            ? ""
+            : pass === checkPass
+            ? "비밀번호가 일치합니다."
+            : "비밀번호가 일치하지 않습니다."}
         </div>
+        <Input type="text" onChange={setName} placeholder={"이름"} />
+        <Input type="text" onChange={setEmail} placeholder={"이메일"} />
+        <Input type="text" onChange={setBirth} placeholder={"생일"} />
+        <Input type="text" onChange={setPhone} placeholder={"휴대폰번호"} />
+        <Input type="text" onChange={setAddress} placeholder={"주소"} />
+
+        <button
+          onClick={() =>
+            AddUser(id, pass, name, email, birth, phone, address)
+              .then((res) => {
+                console.log(res);
+                alert("성공");
+                navigate("/");
+              })
+              .catch((err) => {
+                if (err.response.status === 500) {
+                }
+                if (err.response.status === 401) {
+                  alert("이미 있는 아이디입니다.");
+                }
+              })
+          }
+        >
+          가입
+        </button>
       </div>
     </div>
   );
--- views/src/utils/addUser.js
+++ views/src/utils/addUser.js
@@ -1,31 +1,24 @@
 import axios from "axios";
 
 const addUser = async (id, pass, name, email, birth, phone, address) => {
-  axios
-    .post(
-      `http://13.125.7.108:8080/api/member/signup`,
+  return axios.post(
+    `http://13.125.7.108:8080/api/member/signup`,
 
-      {
-        loginId: id,
-        loginPw: pass,
-        name: name,
-        email: email,
-        birthday: birth,
-        phone: phone,
-        address: address,
+    {
+      loginId: id,
+      loginPw: pass,
+      name: name,
+      email: email,
+      birthday: birth,
+      phone: phone,
+      address: address,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem("token")}`,
       },
-      {
-        headers: {
-          Authorization: `Bearer ${sessionStorage.getItem("token")}`,
-        },
-      }
-    )
-
-    .catch((err) => {
-      if (err.response.status === 500) {
-        alert("이미 있는 아이디입니다.");
-      }
-    });
+    }
+  );
 };
 
 export default addUser;
